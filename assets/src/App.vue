e3103107499e2219f126181ccabba9111576c94b
Adam Wight <adam.wight@wikimedia.de>
2021-03-06 00:07:20 +0100

    Restore custom fonts

--- assets/src/App.vue
+++ assets/src/App.vue
@@ -25,7 +25,6 @@ export default {
 @use 'assets/legacy.css';
 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
