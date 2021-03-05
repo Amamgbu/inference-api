f0190071117f985643a38b8de3b8d18e89f14902
Adam Wight <adam.wight@wikimedia.de>
2021-03-05 01:21:37 +0100

    Working proxy: dev environment only
    The js server will proxy any URL beginning with "/api" to localhost:5000, the
    default for flask.  This is convenient for development but isn't done like this
    in production.

---
+++ assets/vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    devServer: {
+        proxy: {
+            '^/api': {
+                // TODO: From configuration.
+                target: 'http://localhost:5000',
+                changeOrigin: true,
+                // logLevel: 'debug' 
+            },
+        },
+    },
+};
