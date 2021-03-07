e9fb5b5c794bf3756d3760778728ba6aeaca7784
Adam Wight <adam.wight@wikimedia.de>
2021-03-07 12:46:41 +0100

    LanguageSelector: Implement placeholder animation in vue

--- assets/src/components/LanguageSelector.vue
+++ assets/src/components/LanguageSelector.vue
@@ -1,9 +1,28 @@
 <template>
-  <label class="placeholder"><span class="field_name">Language code -- e.g., en for English</span>
-    <select id="lang">
+  <label class="placeholder"
+    :class="{ off: !isFocused && !selected }"
+    @focusin="isFocused = true"
+    @focusout="isFocused = false"
+  ><span class="field_name">Language code -- e.g., en for English</span>
+    <select
+      id="lang"
+      v-model="selected"
+    >
       <option value=""></option>
       <option value="en">English</option>
       <option value="fr">French</option>
     </select>
   </label>
 </template>
+
+<script>
+export default {
+  name: "LanguageSelector",
+  data: function () {
+    return {
+      isFocused: false,
+      selected: "",
+    };
+  },
+}
+</script>
